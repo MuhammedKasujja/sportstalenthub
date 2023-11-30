a52d5bfa8924dde524264aac215035545d7983bc
Muhammed <al.kasmud.2@gmail.com>
2023-11-30 11:53:55 +0300

    update linter

--- lib/widgets/post_info.dart
+++ lib/widgets/post_info.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 
-class PostInfoWidget extends StatelessWidget{
+class PostInfoWidget extends StatelessWidget {
   const PostInfoWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const SizedBox();
   }
-
 }
